ece692a38666b13492bda39b013ad76eddec5d32
Krzysztof Dąbrowski <krzysdabro@live.com>
2020-10-19 09:38:00 +0200

    Fix ineffectual assignment and lint errors

--- internal/cert/print.go
+++ internal/cert/print.go
@@ -2,24 +2,24 @@ package cert
 
 import (
 	"crypto/x509"
+	"fmt"
 
 	"github.com/krzysdabro/tlscert/internal/print"
 )
 
+// Print prints out single certificate.
 func Print(cert *x509.Certificate, verifyOpts x509.VerifyOptions) {
-	v := certValidity{}
-	_, v.err = cert.Verify(verifyOpts)
+	_, err := cert.Verify(verifyOpts)
 
-	printSingle(cert, v)
+	printSingle(cert, err)
 }
 
+// PrintChain prints out all certificates in chain.
 func PrintChain(cert *x509.Certificate, verifyOpts x509.VerifyOptions) {
-	v := certValidity{}
-	chains := [][]*x509.Certificate{}
-	chains, v.err = cert.Verify(verifyOpts)
+	chains, err := cert.Verify(verifyOpts)
 
 	if len(chains) == 0 {
-		printSingle(cert, v)
+		printSingle(cert, err)
 		return
 	}
 
@@ -34,14 +34,21 @@ func PrintChain(cert *x509.Certificate, verifyOpts x509.VerifyOptions) {
 	}
 }
 
-func printSingle(cert *x509.Certificate, validity certValidity) {
+func printSingle(cert *x509.Certificate, validityErr error) {
 	print.PkixName("Subject", cert.Subject)
 	print.PkixName("Issuer", cert.Issuer)
 	if len(cert.DNSNames) > 0 {
 		print.List("DNS Name", cert.DNSNames)
 	}
-	print.Field("Valid", validity.String())
+	print.Field("Valid", isValid(validityErr))
 	print.Field("Not Valid Before", cert.NotBefore.Local().String())
 	print.Field("Not Valid After", cert.NotAfter.Local().String())
 	print.Field("Serial Number", SerialNumber(cert.SerialNumber))
+}
+
+func isValid(err error) string {
+	if err == nil {
+		return "yes"
+	}
+	return fmt.Sprintf("no (%s)", err.Error())
 }
